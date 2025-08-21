25658ecc08b5f28c84507b64b117be191b400c06
GiovanniMaugeri <giova2000.maugeri@gmail.com>
2025-08-21 11:27:01 +0200

    Added project

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("nav-cross").addEventListener("click", function() {
+    document.querySelector("nav").classList.remove("open");
+}) ;
+document.getElementById("burger-menu").addEventListener("click", function() {
+    document.querySelector("nav").classList.add("open");
+    
+}) ;
