67e2c12214cb009f2e79c05810c5e8af10dcb2bb
GiovanniMaugeri <giova2000.maugeri@gmail.com>
2025-08-22 16:14:13 +0200

    added gokart chat and animation

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.getElementById("burger-menu").addEventListener("click", function() {
     document.querySelector("nav").classList.add("open");
     
 }) ;
+var videoDone = false;
+document.getElementById("gokartchatvideo").addEventListener("ended", function() {
+        console.log("video ended");
+        videoDone = true;
+        document.getElementById("gokartchatpolaroid").classList.add("activated");
+})
+document.getElementById("gokartchat").addEventListener("click", function() {
+    if(!videoDone) {
+        document.getElementById("gokartchatvideo").play();
+    }
+    
+});
