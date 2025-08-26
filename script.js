9281ce101d094ded93db11f0ed93618cc3899569
GiovanniMaugeri <giova2000.maugeri@gmail.com>
2025-08-26 09:36:57 +0200

    Added images to polaroid
    Added images to polaroid,fixed size of big phone chat added scroll animation

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ document.getElementById("gokartchat").addEventListener("click", function() {
     }
     
 });
+window.addEventListener("scroll", function() {
+    if(window.scrollY > 1100 && !videoDone) {
+        console.log("scrollY > 1200");
+        document.getElementById("gokartchatvideo").play();
+    }
+});
