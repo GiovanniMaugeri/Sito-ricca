c47011cf7ff32826d9d93b6019d63cca6afc6a0c
GiovanniMaugeri <giova2000.maugeri@gmail.com>
2025-08-27 12:05:14 +0200

    Added more chats and made them responsive

--- script.js
+++ script.js
@@ -50,24 +50,34 @@ document.getElementById("burger-menu").addEventListener("click", function() {
     
 }) ;
 // Video play and polaroid animation
-var videoDone = false;
-document.getElementById("chatVideoGoKart").addEventListener("ended", function() {
-        console.log("video ended");
-        videoDone = true;
-        document.getElementById("gokartchatpolaroid").classList.add("activated");
-})
-// Play video on click
-document.getElementById("gokartchat").addEventListener("click", function() {
-    if(!videoDone) {
-        document.getElementById("chatVideoGoKart").play();
-    }
-    
-});
+// flags = videoGoKartDone, videoSpottedDone, videoPaintballDone
+// implementing with array to make a reference pass in js
+let flags = [false, false, false];
+document.getElementById("chatVideoGoKart").addEventListener("ended", (e)=> videoEnded(e,flags,0))
+document.getElementById("chatVideoSpotted").addEventListener("ended", (e)=> videoEnded(e,flags,1))
+document.getElementById("chatVideoPaintball").addEventListener("ended", (e)=> videoEnded(e,flags,2))
+
+
+function videoEnded(target, flag,index){
+    flag[index] = true;
+    target.target.parentElement.childNodes[1].classList.add("activated");
+
+    console.log(flags);
+}
 // Play video when scrolling to a certain point
 window.addEventListener("scroll", function() {
-    if(window.scrollY > 1100 && !videoDone) {
+    if(window.scrollY > 1100 && !flags[0]) {
         document.getElementById("chatVideoGoKart").muted = true;
         document.getElementById("chatVideoGoKart").play();
+    }
+    if(window.scrollY > 1100 && !flags[1]) {
+        document.getElementById("chatVideoSpotted").muted = true;
+        document.getElementById("chatVideoSpotted").play();
+
+    }
+    if(window.scrollY > 1100 && !flags[2]) {
+        document.getElementById("chatVideoPaintball").muted = true;
+        document.getElementById("chatVideoPaintball").play();
     }
 });
 // Scroll to top on page load
